bd9c8c5e76a9f6a5d747a7305170c2c69c20a50a
ChristopherRios7 <crios9694@gmail.com>
2017-12-06 23:08:58 -0600

    New Rating System

---
+++ rate.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/HealthyTomatoes.Master" AutoEventWireup="true" CodeBehind="rate.aspx.cs" Inherits="HealthyTomatoes.rate" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" href="assets/css/index.css" />
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="cph_Links" runat="server">
+    <li><asp:HyperLink ID="hl_login" runat="server"> <asp:Label ID="lbl_Link" runat="server" Text="Label"></asp:Label> </asp:HyperLink></li>
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="cph_Login" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content4" ContentPlaceHolderID="cph_Main" runat="server">
+    <div class="movieContainer">
+        <div class="header"> 
+            <h1> <asp:Label ID="lbl_header" runat="server" Text="Rate Movie"></asp:Label> </h1> 
+        </div>
+        <div class="title">
+            <asp:Label ID="lbl_movieTitle" runat="server"></asp:Label>
+        </div>
+        <asp:Image ID="movie_Image" runat="server" Height="300" Width="200"/>
+        <div class="rating">
+            <p> <asp:Label ID="lbl_movieRatings" runat="server"></asp:Label> </p>
+            <p> 
+                <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                    <asp:ListItem>1</asp:ListItem>
+                    <asp:ListItem>2</asp:ListItem>
+                    <asp:ListItem>3</asp:ListItem>
+                    <asp:ListItem>4</asp:ListItem>
+                </asp:DropDownList>
+            </p>
+        </div>
+        
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:MovieDBConnectionString %>" SelectCommand="SELECT [rating] FROM [RATINGS]"></asp:SqlDataSource>
+    <br />
+    <asp:Label ID="lblRatingStatus" runat="server" Text=""></asp:Label>
+        <div class="actionRate">
+            <asp:Label ID="lbl_rate" runat="server"></asp:Label>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ rate.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+namespace HealthyTomatoes
+{
+    public partial class rate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string username = (String)Session["Username"];
+            string movie_id = (String)Session["MovieId"];
+
+            string movie_title = (String)Session["MovieTitle"];
+            string movie_rating = (String)Session["MovieRating"];
+            string movie_img_src = (String)Session["MovieImgSrc"];
+
+            Boolean loggedIn = false;
+
+            if (username == "" || username == null) // Not logged in.
+            {
+                loggedIn = false;
+                lbl_Link.Text = "Log In";
+                hl_login.NavigateUrl = "login.aspx";
+            }
+            else
+            {
+                loggedIn = true;
+                lbl_Link.Text = "Welcome, " + username;
+                hl_login.NavigateUrl = "";
+            }
+
+
+            /**
+             * Make sure user is logged in before rate is allowed.
+             */
+            if (loggedIn)
+            {
+                lbl_header.Text = "Rate Movie";
+
+                lbl_movieTitle.Text = movie_title;
+                lbl_movieRatings.Text = "Average User Rating: " + movie_rating + " / 4 stars";
+                movie_Image.ImageUrl = movie_img_src;
+            }
+            else // Tell user to log in.
+            {
+                lbl_header.Text = "You must be logged in to rate a movie!";
+                movie_Image.Visible = false;
+                lbl_movieTitle.Visible = false;
+                lbl_movieRatings.Visible = false;
+                lbl_rate.Visible = false;
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string rating = DropDownList1.SelectedValue;
+            int usrRating = Convert.ToInt32(rating);
+            string insertRating = "UPDATE RATINGS SET rating = " + rating + " WHERE m_id = " + (String)Session["MovieId"] + ";";
+            SqlConnection db = new SqlConnection(SqlDataSource1.ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            db.Open();
+            try
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = insertRating;
+                cmd.Connection = db;
+                cmd.ExecuteNonQuery();
+                Response.Redirect("/rate.aspx");
+            }
+
+            catch (Exception exception)
+            {
+                lbl_movieRatings.Text = exception.ToString();
+            }
+        }
+    }
+}
